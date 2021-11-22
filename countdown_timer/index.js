edcaa432d044407eaf1477980976319b62330127
unknown <dorayaki.sk2.we301@gmail.com>
2021-11-22 22:59:56 +0700

    First commit

---
+++ countdown_timer/index.js
@@ -0,0 +1,33 @@
+(()=>{
+    const second = 1000;
+    const minute = second*60;
+    const hour = minute*60;
+    const day = hour*24;
+
+    function setElementInnerText(id, text){
+        const element = document.getElementById(id);
+        element.innerText = text;
+    }
+    
+    function countdown(){
+        const now = new Date().getTime();
+        const newYear = new Date('December 31, 2021 23:59:59').getTime();
+        const unixTimeLeft = newYear-now;
+        console.log(unixTimeLeft);
+        // const daysElem = document.getElementById('days');
+        // daysElem.innerText = Math.floor(unixTimeLeft/day);
+        setElementInnerText('days',Math.floor(unixTimeLeft/day));
+        setElementInnerText('hours', Math.floor(unixTimeLeft % day / hour));
+        setElementInnerText('minutes', Math.floor(unixTimeLeft % hour / minute));
+        setElementInnerText('seconds', Math.floor(unixTimeLeft % minute / second));
+    }
+    function run(){
+        setInterval(() => {
+            countdown();    
+        }, second);
+        
+        
+    }
+    run();
+
+})();
---
+++ falling_snow/js1.js
@@ -0,0 +1,66 @@
+(()=>{
+
+    function setup(){
+        const canvas = document.getElementById('falling-snow-canvas');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        return{
+            canvas,
+            canvasContext: canvas.getContext('2d'),
+            numberOfSnowBalls: 250 
+        }
+    }
+
+    function random(min, max){
+        return Math.floor(Math.random() * (max-min+1))+min;
+    }
+
+    function createSnowBalls(canvas, numberOfSnowBalls){
+        return [...Array(numberOfSnowBalls)].map(()=>{
+           return {
+               x: random(0, canvas.width),
+               y: random(0, canvas.height),
+               opacity: random(0.5,1),
+               radius: random(2,4),
+               speedX: random(-5, 5),
+               speedY: random(1, 3) 
+           };
+        });
+       //console.log(x);
+    }
+
+    function drawSnowBalls(canvasContext, snowBalls){
+        canvasContext.beginPath();
+        canvasContext.arc(snowBalls.x, snowBalls.y, snowBalls.radius, 0, Math.PI*2);
+        canvasContext.fillStyle = `rgba(255,255,255, ${snowBalls.opacity})`;
+        canvasContext.fill();
+    }
+
+    function moveSnowBalls(canvas ,snowBalls){
+        snowBalls.x += snowBalls.speedX;
+        snowBalls.y += snowBalls.speedY;
+        if(snowBalls.x > canvas.width){
+            snowBalls.x = 0;
+        }else if(snowBalls.x < 0){
+            snowBalls.x = canvas.width;
+        }
+
+        if(snowBalls.y > canvas.height){
+            snowBalls.y = 0;
+        }
+
+    }
+    function run(){
+        const {canvas, canvasContext, numberOfSnowBalls} = setup();
+        const snowBalls = createSnowBalls(canvas, numberOfSnowBalls);
+        console.log(snowBalls);
+        //drawSnowBalls(canvasContext, snowBalls[0]);
+        
+        setInterval(() => {
+            canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+            snowBalls.forEach((snowBalls)=> drawSnowBalls(canvasContext, snowBalls));
+            snowBalls.forEach((snowBalls)=> moveSnowBalls(canvas,snowBalls));    
+        }, 50);
+    }
+    run();
+})();
